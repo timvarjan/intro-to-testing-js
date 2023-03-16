e27c0bf829f95d3c74154c32d4102f419d86c1fa
timvarjan <127249482+timvarjan@users.noreply.github.com>
2023-03-16 13:52:21 -0500

    Problem 8 complete

--- tests.js
+++ tests.js
@@ -26,7 +26,7 @@ describe('sayHello', function() {
     });
 })
 
-//3, 4, 5,6
+//3, 4, 5, 6, 7, 8
 describe('sayHello', function() {
     it('should return Hello, Jane!', function() {
         expect(sayHello('Jane')).toBe('Hello, Jane!');
@@ -37,4 +37,8 @@ describe('sayHello', function() {
     it('should return Hello, Pat!', function() {
         expect(sayHello('Pat')).toBe('Hello, Pat!');
     });
+    it('should return Hello, World!', function() {
+        expect(sayHello('World')).toBe("Hello, World!");
+    });
 })
+
